89c03fea85d7ddf0a98cc953a8bbb681d168998f
CrashLaker <crashlaker@gmail.com>
2022-09-13 02:20:41 -0400

    ok

---
+++ d3js-static/jsaudio.js
@@ -0,0 +1,17 @@
+navigator.getUserMedia = navigator.getUserMedia ||navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+    
+var aCtx;
+var analyser;
+var microphone;
+if (navigator.getUserMedia) {
+  navigator.getUserMedia(
+    {audio: true}, 
+    function(stream) {
+      aCtx = new AudioContext();
+      microphone = aCtx.createMediaStreamSource(stream);
+      var destination=aCtx.destination;
+      microphone.connect(destination);
+    },
+    function(){ console.log("Error 003.")}
+  );
+}
